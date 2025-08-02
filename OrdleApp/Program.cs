7df04c453ef3309a59471c8fddcafdaa447a59ed
Jacob Gandemo <122456645+jacob-gandemo-matchi@users.noreply.github.com>
2025-08-02 16:07:05 +0200

    Add working ordle game

--- OrdleApp/Program.cs
+++ OrdleApp/Program.cs
@@ -6,6 +6,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+var fiveLetterWords = Get5LetterWordsList();
+builder.Services.AddSingleton(fiveLetterWords);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -25,3 +28,22 @@ app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
 app.Run();
+
+
+HashSet<string> Get5LetterWordsList()
+{
+    // Path to the cleaned file
+    var dataFolder = Path.Combine(Directory.GetCurrentDirectory(), "Data");
+    var saolOutputPath = Path.Combine(dataFolder, "SAOL_5Words_Clean.txt");
+
+    if (!File.Exists(saolOutputPath))
+    {
+        return new HashSet<string>();
+    }
+
+    var words = File.ReadAllLines(saolOutputPath)
+        .Select(w => w.Trim().ToUpper())
+        .ToHashSet();
+
+    return words;
+}
